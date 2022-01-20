6736477306c75d0908aa7ce45ab3ffba23cfc07f
Nikita <saininikita23@gmail.com>
2022-01-20 14:39:26 +0530

    context value

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,8 +6,8 @@ class Button extends React.Component {
     static contextType=LanguageContext;
 
     render() {
-        console.log(this.context);
-        return <button className="ui primary button">Submit</button>
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        return <button className="ui primary button">{text}</button>
     }
 }
 
