9e8edff355b70732ce5d4db71b7a8812c11611fd
Nikita <saininikita23@gmail.com>
2022-01-20 15:39:44 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
-
+import ColorContext from "../contexts/ColorContext";
 class App extends React.Component {
     state ={ language: 'english'};
 
@@ -18,10 +18,12 @@ class App extends React.Component {
                 onClick = {() => this.onLanguageChange('english')} />
                 <i className="flag nl" onClick = {() => this.onLanguageChange('dutch')} />
             </div>
+            <ColorContext.Provider value = "red">
             <LanguageContext.Provider value={this.state.language}>
+            
             <UserCreate />
             </LanguageContext.Provider>
-            
+            </ColorContext.Provider>
         </div>
         );
     }
