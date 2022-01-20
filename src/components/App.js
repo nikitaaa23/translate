dfa89945d42d81325f03b8c5ed68399dbe775e72
Nikita <saininikita23@gmail.com>
2022-01-20 16:07:05 +0530

    rendering language store

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import {LangaugeStore} from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-    state ={ language: 'english'};
-
-
-    onLanguageChange = language => {
-        this.setState({ language});
-    }
+ 
     render() {
         return (
         <div className="ui container">
-            <LanguageSelector onLanguageChange={this.onLanguageChange} />
+            <LangaugeStore>
+            <LanguageSelector  />
             <ColorContext.Provider value = "red">
-            <LanguageContext.Provider value={this.state.language}>
-            
+           
             <UserCreate />
-            </LanguageContext.Provider>
             </ColorContext.Provider>
+            </LangaugeStore>
         </div>
         );
     }
